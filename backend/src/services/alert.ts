ab2ec694ba4471335ca46edbe0a090d70907481b
priyanshunitr <sahupriyanshu2006@gmail.com>
2026-04-17 02:28:06 +0530

    created alert and sos service functions

---
+++ backend/src/services/alert.ts
@@ -0,0 +1,46 @@
+import pool from "../lib/dbConnect.js";
+import {CreateAlertInput} from "../schemas/alertsSchema.js";
+
+export async function createAlert(
+  userId: string,
+  payload: Omit<CreateAlertInput, "created_by" | "is_broadcast">,
+) {
+  const {
+    emergency_session_id,
+    created_by,
+    severity,
+    title,
+    description,
+    location_text,
+    is_broadcast,
+  } = {...payload,
+    created_by: userId,
+    is_broadcast: false}
+
+  const result = await pool.query(
+    `
+        INSERT INTO alerts (
+          emergency_session_id,
+          created_by,
+          severity,
+          title,
+          description,
+          location_text,
+          is_broadcast
+        )
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
+        RETURNING *
+      `,
+    [
+      emergency_session_id ?? null,
+      created_by ?? null,
+      severity,
+      title,
+      description,
+      location_text ?? null,
+      is_broadcast,
+    ],
+  );
+
+  return result.rows[0];
+}
---
+++ backend/src/services/sos.ts
@@ -0,0 +1,43 @@
+import { error } from "node:console";
+import pool from "../lib/dbConnect.js";
+import { CreateSosRequestInput } from "../schemas/sosRequestSchema.js";
+
+export async function createSosRequest(userID:string, payload: Omit<CreateSosRequestInput, "guest_user_id">) {
+
+    const {
+        emergency_session_id,
+        guest_user_id,
+        message,
+        latitude,
+        longitude,
+        location_accuracy_m,
+        location_captured_at,
+    } = {...payload, guest_user_id: userID };
+
+    const result = await pool.query(
+        `
+            INSERT INTO sos_requests (
+                emergency_session_id,
+                guest_user_id,
+                message,
+                latitude,
+                longitude,
+                location_accuracy_m,
+                location_captured_at
+            )
+            VALUES ($1, $2, $3, $4, $5, $6, $7)
+            RETURNING *
+        `,
+        [
+            emergency_session_id ?? null,
+            guest_user_id,
+            message ?? null,
+            latitude ?? null,
+            longitude ?? null,
+            location_accuracy_m ?? null,
+            location_captured_at ?? null,
+        ],
+    );
+
+    return result.rows[0];
+}
